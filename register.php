93fa0fbd1a0fc4c7e8449466e1915f9eea281ffc
swap22 <sharma.sswapnil@gmail.com>
2017-10-21 14:54:35 +0530

    to see the blog and contacting us user need to register first through this page

---
+++ register.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="simple template for a shop with blog post as option">
+    <meta name="author" content="Swapnil Sharma">
+
+    <title>Simple website-Register</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!--header for the website -->
+        <?php require_once("header.php")?>
+
+    <!-- Navigation -->
+    <?php require_once("topNav.php")?>
+
+    <div class="container">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Registration
+                        <strong>form</strong>
+                    </h2>
+					<div id="add_err2"></div>
+                    <hr>       
+                    <form role="form">
+                        <div class="row">
+                            <div class="form-group col-lg-4">
+                                <label>First Name</label>
+                                <input type="text" id="fname" name="fname" maxlength="25" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-4">
+                                <label>Last Name</label>
+                                <input type="text" id="lname" name="lname" maxlength="25" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-4">
+                                <label>Email Address</label>
+                                <input type="email" id="email" name="email" maxlength="25" class="form-control">
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="form-group col-lg-12">
+                                <label>Password</label>
+                                <input type="password" id="password" name="password" maxlength="10" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <button type="submit" id="register" class="btn btn-default">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <?php require_once("footer.php")?>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
